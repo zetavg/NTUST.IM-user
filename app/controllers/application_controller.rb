74e7f5f7d6a1c7856a06bebe4179d2779337edbf
Neson <neson@dex.tw>
2014-08-17 01:35:04 +0800

    setup seo_helper and view layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :get_app_setting
+
+  private
+
+  def get_app_setting
+    @google_analytics_id = Setting.google_analytics_id
+  end
 end
